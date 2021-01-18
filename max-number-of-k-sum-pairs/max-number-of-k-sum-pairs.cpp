013a1d7ab410841addc38a023acc9ad51eb94907
Anu Kumari Gupta <jyotianugpt@gmail.com>
2021-01-18 23:52:42 +0530

    Time: 400 ms (52.86%), Space: 74.1 MB (14.32%) - LeetHub

---
+++ max-number-of-k-sum-pairs/max-number-of-k-sum-pairs.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int maxOperations(vector<int>& nums, int k) {
+        
+        map<int, int> mp;
+        int ans = 0;
+        
+        for(int i = 0; i < nums.size(); i++){
+            if(mp[k - nums[i]] > 0){
+               mp[k - nums[i]]--;
+                ans++;
+            }
+            else{
+                mp[nums[i]]++;
+            }
+        }
+        return ans;
+    }
+};
